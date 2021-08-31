f8a9c1724cd47d7757dfa2db32d52cb637550154
Shruti Jaigariya <72396908+shrutijai1@users.noreply.github.com>
2021-09-01 02:46:25 +0700

    Print of addition of two numbers

---
+++ Basic_1.py
@@ -0,0 +1,10 @@
+#print addition of two numbers
+
+number1 = int(input("Enter first number : ")) # taking input form user for first number1
+number2 = int(input("Enter first number : ")) # taking input form user for second number2
+print("addition of two number is", number1+number2)
+sum = number1+number2
+
+#second way using fromating 
+print("Sum of {0} and {1} is {2}" .format(number1, number2, sum) )
+print("Sum of {0} and {1} is {2}".format(number1, number2, sum))
